7dbe38d041e4119a9eebefb0d4ac693ed34eaf45
kkanto <108447856+kkanto@users.noreply.github.com>
2022-11-28 13:28:40 +0100

    Create maven project

---
+++ src/main/java/de/allianz/ullamco/UllamcoApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.ullamco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UllamcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UllamcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/ullamco/UllamcoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.ullamco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UllamcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
